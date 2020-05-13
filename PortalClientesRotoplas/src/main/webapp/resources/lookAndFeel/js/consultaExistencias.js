4a8d97a577f789a2afa5d10f799e9205cbe0382d
ALEXISSOLIS <ALEXISdev_1996@outlook.com>
2020-05-13 02:43:53 -0500

    contactanos.js contactanos.xhtml

--- PortalClientesRotoplas/src/main/webapp/resources/lookAndFeel/js/consultaExistencias.js
+++ PortalClientesRotoplas/src/main/webapp/resources/lookAndFeel/js/consultaExistencias.js
@@ -65,41 +65,35 @@ function reload(){
 
 
 function filtrar() {
-    if(document.getElementById("form:frm_noMaterial").value == "" && document.getElementById("form:frm_noMaterial").value.length == 0){
-        showToastr('El No. de Material es obligatorio.', 'Aviso', {
-            type: typeNotification.warning
-        })
-    }else{
-        var table = $('[id="form:productois"]').DataTable();
-        table.destroy();
-        $('.collapse').collapse('hide');
-        $('[id="form:productois"]').DataTable({
-            paging: true,
-            searching: true,
-            lengthChange: false,
-            scrollY: true,
-            "scrollX": true,
-            language: {
-                "decimal": "",
-                "emptyTable": "No hay información",
-                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-                "infoPostFix": "",
-                "thousands": ",",
-                "lengthMenu": "Mostrar _MENU_ Entradas",
-                "loadingRecords": "Cargando...",
-                "processing": "Procesando...",
-                "search": "Buscar:",
-                "zeroRecords": "Sin resultados encontrados",
-                "paginate": {
-                    "first": "Primero",
-                    "last": "Ultimo",
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            },})
-    }
+    var table = $('[id="form:productois"]').DataTable();
+    table.destroy();
+    $('.collapse').collapse('hide');
+    $('[id="form:productois"]').DataTable({
+        paging: true,
+        searching: true,
+        lengthChange: false,
+        scrollY: true,
+        "scrollX": true,
+        language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },})
 }
 
 function tablaMessage(){
